b11cc54bd5b3e692706ba02d00ee0c5b8b83894d
Nick Ivanukov <ivanukov78@gmail.com>
2024-02-26 18:52:41 +0400

    refactor trigger filter

--- app/filter/filter_message.py
+++ app/filter/filter_message.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from re import findall
 
 from aiogram.types import Message
 from aiogram.filters import BaseFilter
@@ -13,7 +14,7 @@ class TriggerFilter(BaseFilter):
 
 
 async def check(message: Message) -> dict[str, str]:
-    for trigger_key in ARCH_WORDS.keys():
-        for trigger_word in ARCH_WORDS[trigger_key]:
-            if trigger_word in message.text:
-                return {"trigger": trigger_key}
+    for trigger_key in ARCH_WORDS:
+        pattern = rf'{trigger_key}\w+'
+        if findall(pattern, message.text):
+            return {"trigger": trigger_key}
