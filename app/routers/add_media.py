4aa438a330ad298b6000548da7da9c4932a3365e
Nick Ivanukov <ivanukov78@gmail.com>
2024-06-25 10:14:23 +0300

    refactor add media

--- app/routers/add_media.py
+++ app/routers/add_media.py
@@ -8,9 +8,9 @@ from aiogram.fsm.context import FSMContext
 from fast_depends import inject, Depends
 
 from app.fsm.add_photo_state import AddPhotoState
-from app.common.marker.gateway import TransactionGatewayMarker
+from app.common.marker import TransactionGatewayMarker
 from app.database.core.gateway import DatabaseGateway
-from app.filter.is_admin import IsAdmin
+from app.filter import IsAdmin, ContentFilter
 from app.core.settings import PATH_TO_HOME
 
 PATH_TO_FOLDER_PHOTO = str(PATH_TO_HOME) + "/media"
@@ -18,13 +18,13 @@ PATH_TO_FOLDER_PHOTO = str(PATH_TO_HOME) + "/media"
 add_router = Router(name=__name__)
 
 
-@add_router.message(Command("add_photo"),
+@add_router.message(Command("add"),
                     IsAdmin(),
                     StateFilter(default_state)
                     )
 async def entry_photo(message: Message,
                       state: FSMContext):
-    await message.answer(text="""Начнём же добавлять. Введите слово, на которое будет реагировать бот\nВведите /stop_photo ,чтобы остановить добавление фотографии""")
+    await message.answer(text="""Начнём же добавлять. Введите слово, на которое будет реагировать бот\nВведите /stop, чтобы остановить процесс добавления медиа""")
     await state.set_state(AddPhotoState.add_trigger)
 
 
@@ -41,23 +41,29 @@ async def add_trigger(message: Message,
         await message.answer(f"Слово {trigger_word} уже занято")
     else:
         await state.update_data(name=trigger_word)
-        await message.answer(text=f"""хорошо, вы установили {trigger_word}.\nТеперь хотелось бы увидеть фотографию.\nОтправьте же мне ее""")
-        await state.set_state(AddPhotoState.add_photo)
+        await message.answer(text=f"""Хорошо, вы установили {trigger_word}.\nТеперь хотелось бы увидеть медиа.\nОтправьте же мне ее""")
+        await state.set_state(AddPhotoState.add_media)
 
 
-@add_router.message(F.photo, StateFilter(AddPhotoState.add_photo))
+@add_router.message(ContentFilter, StateFilter(AddPhotoState.add_media))
 @inject
 async def add_photo(message: Message,
-                    bot: Bot,
+                    trigger_type: str,
                     gateway: Annotated[DatabaseGateway, Depends(TransactionGatewayMarker)],
                     state: FSMContext):
 
-    path_photo = f"{PATH_TO_FOLDER_PHOTO}/{message.photo[-1].file_id}.jpg"
-    await bot.download(message.photo[-1],
-                       destination=path_photo
-                       )
-    image_from_url = FSInputFile(path_photo)
-    await state.update_data(url=image_from_url.path)
+    if trigger_type == "photo":
+        media_id = message.photo.id
+    elif trigger_type == "audio":
+        media_id = message.audio.file_id
+    elif trigger_type == "video":
+        media_id = message.video.file_id
+    elif trigger_type == "voice":
+        media_id = message.voice.file_id
+    else:
+        media_id = message.sticker.file_id
+
+    await state.update_data(media_id=media_id)
     data = await state.get_data()
     media_repr = gateway.media()
     await media_repr.create(**data)
